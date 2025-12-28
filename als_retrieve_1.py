d5488cf38f8f2084d4e1f9b420534f223c48f593
Ana <ana.vajic@etu.unistra.fr>
2025-12-28 14:21:41 +0100

    normalise variable names across the scripts

--- als_retrieve_1.py
+++ als_retrieve_1.py
@@ -7,20 +7,21 @@ def tr_retrieve(word, tr):
 
 def match_search():
     dataset = pd.read_excel("german-french.ods")
-    alsatian_aligned = pd.read_excel("alsatian-alignments.ods")
+    alsatian_dict = pd.read_excel("alsatian-alignments.ods")
     dataset["Alsatian"] = "."
 
     for i in range(len(dataset)):
-        for j in range(len(alsatian_aligned)):
+        for j in range(len(alsatian_dict)):
             wg = dataset.loc[i]["German"].lower()
-            tg = alsatian_aligned.loc[j]["German"].lower().split(";")
+            tg = alsatian_dict.loc[j]["German"].lower().split(";")
             wf = dataset.loc[i]["French"].lower()
-            tf = alsatian_aligned.loc[j]["French"].lower().split(";")
+            tf = alsatian_dict.loc[j]["French"].lower().split(";")
 
             if tr_retrieve(wg, tg):
-                dataset.loc[i,"Alsatian"] = alsatian_aligned.loc[j]["Alsatian"]
+                dataset.loc[i,"Alsatian"] = alsatian_dict.loc[j]["Alsatian"]
             if tr_retrieve(wf, tf):
-                dataset.loc[i,"Alsatian"] = alsatian_aligned.loc[j]["Alsatian"]
+                dataset.loc[i,"Alsatian"] = alsatian_dict.loc[j]["Alsatian"]
+    return dataset
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import pandas as pd
 from als_retrieve_1 import  tr_retrieve, match_search
 from als_retrieve_2_1 import clean
 from cognates_scores import add_pronunciation, orth_score, pron_score
+
 
 match_search()
 add_pronunciation()
