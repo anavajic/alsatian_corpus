24b39195ff329f59b1f41ec52457975fec4c94c5
Ana <ana.vajic@etu.unistra.fr>
2026-01-07 16:08:14 +0100

    add test

--- tests/test_pron_score.py
+++ tests/test_pron_score.py
@@ -1,4 +1,8 @@
 import pytest
+import cognates_scores
 
 def test_pron_score():
-    pass
+    pron1 = ('APSNS', '')
+    pron2 = ('APSNS', '')
+    result = cognates_scores.pron_score(pron1, pron2)
+    assert result == 1.0
